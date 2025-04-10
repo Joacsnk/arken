f28d58129ef1b494fc7a66e4d8d19c1b488bb385
Joacsnk <Joacsnk@gmail.com>
2025-04-10 12:34:28 -0300

    style: adição de interface funcional
    - adição de interface com 2 opções, sendo a de "criar senha" e "sair" (funcional)
    
    - sistema de erros caso opção errada
    
    - defs de limpeza do terminal e erro 1

---
+++ main.py
@@ -0,0 +1,37 @@
+from os import system as sy
+from time import sleep as sl
+class main():
+    
+    def inicio(self):
+        self.limpar_terminal()
+        self.selecao(self.interface())
+    
+    def limpar_terminal(self):
+        sy("cls")
+    
+    def opcao_inválida(self):
+        self.limpar_terminal()
+        sl(1)
+        print("Erro 1. Opção inválida. Verifique se digitou corretamente.")
+        sl(2)
+        self.limpar_terminal()
+        self.inicio()
+    
+    def interface(self):
+        print("✦  — — — — — ARKEN— — — — —  ✦\n\n")
+        print("[1] - Criar senha\n[2] - Sair\n")
+        return str(input("Digite a opção desejada: "))
+    
+    def selecao(self, opcao):
+        match opcao:
+            case "2":
+                self.limpar_terminal()
+                exit()
+            case _:
+                self.opcao_inválida()
+                
+            
+    
+if __name__ == "__main__":
+    app = main()
+    app.inicio()
